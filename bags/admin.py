667a794ca4b9a6ceeef468867abbef29d00be834
hamid <hamedreza1992@gmail.com>
2024-09-16 10:42:32 +0330

    create app model order and admin order items

--- bags/admin.py
+++ bags/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Order
+from .models import Order, OrderItem
 
 
 # Register your models here.
@@ -8,4 +8,12 @@ class OrderAdmin(admin.ModelAdmin):
     model = Order
     list_display = ('id', 'user', 'is_paid', 'on_delete')
     search_fields = 'user'
-    list_filter = ('is_paid','on_delete')
+    list_filter = ('is_paid', 'on_delete')
+
+
+@admin.register(OrderItem)
+class OrderItemInLine(admin.TabularInline):
+    model = OrderItem
+    extra = 1
+    max_num = 1
+    min_num = 1
--- bags/models.py
+++ bags/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from product.models import Product
 
 
 # Create your models here.
@@ -14,3 +15,17 @@ class Order(models.Model):
     class Meta:
         verbose_name = 'Order'
         verbose_name_plural = 'Orders'
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    product_count = models.IntegerField(default=1)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.product.name} - {self.product_count} - {self.total_price}'
+
+    class Meta:
+        verbose_name = 'Order item'
+        verbose_name_plural = 'Order items'
