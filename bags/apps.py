5166a0ab5232599b1571a4ee8d3c8022b69cc2a1
hamid <hamedreza1992@gmail.com>
2024-09-16 09:54:57 +0330

    create app bags

---
+++ bags/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BagsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'bags'
--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "core",
     "product",
     "account",
+    "bags"
 
     # extra app
     "rest_framework",
