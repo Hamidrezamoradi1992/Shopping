47da0ea2ee84f478a89eee2bfddb6043a73e2c81
hamid <hamedreza1992@gmail.com>
2024-09-16 10:30:01 +0330

    create app model order and admin order

--- bags/admin.py
+++ bags/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Order
+
 
 # Register your models here.
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    model = Order
+    list_display = ('id', 'user', 'is_paid', 'on_delete')
+    search_fields = 'user'
+    list_filter = ('is_paid','on_delete')
--- bags/models.py
+++ bags/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField(null=True, blank=True)
+    is_paid = models.BooleanField(default=False)
+    on_delete = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user
+    class Meta:
+        verbose_name = 'Order'
+        verbose_name_plural = 'Orders'
